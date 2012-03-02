f9a4a15577f8f6a0beff0304b7f99a5de1507571
Noah Daniels <ndaniels@mac.com>
2012-03-02 11:06:36 -0500

    clean up types, rewrite intro, tweak diagrams, add alignment diagram

--- viterbi_types.hs
+++ viterbi_types.hs
@@ -1,27 +1,25 @@
 type HMMState = Int
 type StatePath = [ HMMState ]
-mat = 0 :: HMMState
-ins = 1 :: HMMState
-del = 2 :: HMMState
-type EmissionProbabilities = V.Vector Double
-data TransitionProbability (fState::HMMState, tState::HMMState) = 
-     TransitionProbability { logProbability::LogProbability
-                           , fromState = value fState::HMMState
-                           , toState = value tState::HMMState
-                           }
-data TransitionProbabilities =
-     TransitionProbabilities { m_m :: TransitionProbability
-                             , m_i :: TransitionProbability
-                             , m_d :: TransitionProbability
-                             , i_m :: TransitionProbability
-                             , i_i :: TransitionProbability
-                             , d_m :: TransitionProbability
-                             , d_d :: TransitionProbability
-                             }
+data HMMState = Match | Insertion | Deletion
+type EProbs = V.Vector Double
+data TProb  = 
+     TProb { logProbability::LogProbability
+           , fromState::HMMState
+           , toState::HMMState
+           }
+data TProbs =
+     TProbs { m_m :: TProb
+            , m_i :: TProb
+            , m_d :: TProb
+            , i_m :: TProb
+            , i_i :: TProb
+            , d_m :: TProb
+            , d_d :: TProb
+            }
 data HmmNode =
      HmmNode { nodeNum :: Int
-             , matchEmissions :: EmissionProbabilities
-             , insertionEmissions :: EmissionProbabilities
-             , transitions :: TransitionProbabilities
+             , matchEmissions :: EProbs
+             , insertionEmissions :: EProbs
+             , transitions :: TProbs
              }
 type HMM = V.Vector HmmNode
